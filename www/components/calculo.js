1af3684288f35a1b16d08d0959e6746d65b08cd7
camisnet <felixcamily6@gmail.com>
2021-04-09 05:58:17 +0900

    calculadora

---
+++ www/components/calculo.js
@@ -0,0 +1,50 @@
+// This is a JavaScript file
+var textoDisplay = "";
+
+var valor1 = 0;
+
+var operador = "";
+
+function zerarDisplay(){
+  textoDisplay = "";
+  $(".display").val("");
+}
+
+$(document).on("click","#c", function(){
+    zerarDisplay();
+    valor1 = 0;
+})
+
+$(document).on("click",".botao", function(){
+    textoDisplay += $(this).val();
+    $(".display").val(textoDisplay);
+})
+
+$(document).on("click",".ope",function(){
+  operador = $(this).html();
+  valor1 = textoDisplay;
+  zerarDisplay();
+})
+
+$(document).on("click", "#igual", function(){
+  var valor2 = textoDisplay;
+  zerarDisplay();
+
+  if(operador == "+"){
+   textoDisplay = parseFloat(valor1) + parseFloat(valor2);
+  }
+
+   if(operador == "-"){
+   textoDisplay = parseFloat(valor1) - parseFloat(valor2);
+  }
+
+   if(operador == "*"){
+   textoDisplay = parseFloat(valor1) * parseFloat(valor2);
+  }
+
+   if(operador == "/"){
+   textoDisplay = parseFloat(valor1) / parseFloat(valor2);
+  }
+
+   $(".display").val(textoDisplay);
+})
